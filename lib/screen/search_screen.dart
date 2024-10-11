a04f60a12b22e509e3a9358da5125fd9dca703bb
Royhan <royhankeren12@gmail.com>
2024-10-11 20:44:13 +0700

    Search screen -update

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:application_music/style/stylesheet.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -11,12 +12,256 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Search Screen",
+      body: ListView(
+        padding: const EdgeInsets.all(
+          20,
         ),
-        backgroundColor: Colors.cyan,
+        children: [
+          header(),
+          body(),
+        ],
       ),
+    );
+  }
+
+  Widget header() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(
+            10,
+          ),
+        ),
+        contentPadding: const EdgeInsets.all(
+          12,
+        ),
+        prefixIcon: const Icon(
+          Icons.search,
+          size: 20,
+          color: Colors.grey,
+        ),
+        hintText: "Search songs,artist, album, offical album, relaxing",
+        hintStyle: const TextStyle(
+          overflow: TextOverflow.ellipsis,
+          color: Colors.grey,
+          fontSize: 15,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      keyboardType: TextInputType.name,
+    );
+  }
+
+  Widget body() {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 50,
+        ),
+        Row(
+          children: [
+            Image.asset(
+              "assets/images/trending_up.png",
+              scale: 3,
+              color: Colors.black,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              "Trending Artist",
+              style: txtBlack.copyWith(
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 10,
+                    ),
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Data 1",
+                      style: txtBlack,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 10,
+                    ),
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Data 1",
+                      style: txtBlack,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 10,
+                    ),
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Data 1",
+                      style: txtBlack,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 10,
+                    ),
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Data 1",
+                      style: txtBlack,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 10,
+                    ),
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Data 1",
+                      style: txtBlack,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 10,
+                    ),
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Data 1",
+                      style: txtBlack,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 10,
+                    ),
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Data 1",
+                      style: txtBlack,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
